d2c3d48abc94eec1cdc9da8f39ed355f9ab1825a
Sivdvj <151998553+Sivdvj@users.noreply.github.com>
2026-05-30 22:40:15 +0530

    chore: fix ui for userlist

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -1,27 +1,40 @@
+import { Icon } from "@iconify/react";
 function UserList({ userlist, mysocketId, kickUser }) {
   let me = userlist.find((u) => u.socketId === mysocketId);
   let owner = me?.isOwner;
 
   return (
     <div>
-      Users
-      {userlist.map((user) => (
-        <div key={user.socketId} style={{ display: "flex" }}>
+      <div className="uppercase tracking-wider font-bold mb-4">
+        Collaborators ({userlist.length})
+      </div>
+      <div className="space-y-3">
+        {userlist.map((user) => (
           <div
-            style={{
-              width: "12px",
-              height: "12px",
-              borderRadius: "50%",
-              background: user.color,
-            }}
-          />
-          {user.name}
-          {user.isOwner && <span>Owner</span>}
-          {owner && user.socketId !== mysocketId && (
-            <button onClick={() => kickUser(user.socketId)}>Kick</button>
-          )}
-        </div>
-      ))}
+            key={user.socketId}
+            className="flex items-center justify-between rounded-lg bg-zinc-900 p-2"
+          >
+            <div className="flex gap-3 items-center">
+              <div
+                className="w-5 h-5 rounded-full"
+                style={{ background: user.color }}
+              />
+              <div>{user.name}</div>
+            </div>
+            {user.isOwner && (
+              <Icon icon="lucide:crown" className="w-5 h-5 text-amber-300" />
+            )}
+            {owner && user.socketId !== mysocketId && (
+              <button onClick={() => kickUser(user.socketId)}>
+                <Icon
+                  icon="pepicons-pop:leave"
+                  className="w-5 h-5 text-rose-400"
+                />
+              </button>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
